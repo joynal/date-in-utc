598c7ff29f755c9f59b3862ca611801fffc11afe
Joynal Abedin <joynal3483@gmail.com>
2018-05-19 23:07:44 +0600

    added unit test

---
+++ test.js
@@ -0,0 +1,28 @@
+const test = require('tape');
+const dateInUtc = require('.');
+
+test('convert with utc offset ->> 0', (assert) => {
+  const actual = dateInUtc(0, new Date('2018-4-25 18:30')).toLocaleString();
+  const expected = '2018-4-25 12:30:00';
+
+  assert.equal(actual, expected);
+  assert.end();
+});
+
+
+test('convert with utc offset ->> 5.5', (assert) => {
+  const actual = dateInUtc(5.5, new Date('2018-4-25 18:30')).toLocaleString();
+  const expected = '2018-4-25 18:00:00';
+
+  assert.equal(actual, expected);
+  assert.end();
+});
+
+
+test('convert with utc offset ->> -12', (assert) => {
+  const actual = dateInUtc(-12, new Date('2018-4-25 18:30')).toLocaleString();
+  const expected = '2018-4-25 00:30:00';
+
+  assert.equal(actual, expected);
+  assert.end();
+});
