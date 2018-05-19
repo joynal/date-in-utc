46f3b841818d1704d309d0c19ba366283bc94de8
Joynal Abedin <joynal3483@gmail.com>
2018-05-19 23:18:24 +0600

    improved test case

--- test.js
+++ test.js
@@ -2,27 +2,30 @@ const test = require('tape');
 const dateInUtc = require('.');
 
 test('convert with utc offset ->> 0', (assert) => {
-  const actual = dateInUtc(0, new Date('2018-4-25 18:30')).toLocaleString();
+  const actual = dateInUtc(0, new Date('2018-4-25 18:30'));
   const expected = '2018-4-25 12:30:00';
 
-  assert.equal(actual, expected);
+  assert.equal(actual.toLocaleString(), expected);
+  assert.equal(actual.getTime(), new Date(expected).getTime());
   assert.end();
 });
 
 
 test('convert with utc offset ->> 5.5', (assert) => {
-  const actual = dateInUtc(5.5, new Date('2018-4-25 18:30')).toLocaleString();
+  const actual = dateInUtc(5.5, new Date('2018-4-25 18:30'));
   const expected = '2018-4-25 18:00:00';
 
-  assert.equal(actual, expected);
+  assert.equal(actual.toLocaleString(), expected);
+  assert.equal(actual.getTime(), new Date(expected).getTime());
   assert.end();
 });
 
 
 test('convert with utc offset ->> -12', (assert) => {
-  const actual = dateInUtc(-12, new Date('2018-4-25 18:30')).toLocaleString();
+  const actual = dateInUtc(-12, new Date('2018-4-25 18:30'));
   const expected = '2018-4-25 00:30:00';
 
-  assert.equal(actual, expected);
+  assert.equal(actual.toLocaleString(), expected);
+  assert.equal(actual.getTime(), new Date(expected).getTime());
   assert.end();
 });
